86f2015b1defe4af9c284e0bb010be9ac3848330
hahha <111@.com>
2015-10-08 18:23:47 +0800

    测试准备

---
+++ MyWb/MyWb/Class/Controllers/Main/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  MyWb
+//
+//  Created by 王明申 on 15/10/8.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
