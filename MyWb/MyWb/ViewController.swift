a3910c3b79e0e51b0d21fce31fa455ac75618f3d
hahha <111@.com>
2015-10-08 16:49:46 +0800

    初始化完成

---
+++ MyWb/MyWb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyWb
+//
+//  Created by 王明申 on 15/10/8.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
