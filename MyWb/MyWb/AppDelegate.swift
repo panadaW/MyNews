e221a2f935fe9795afab6f03dcc44325747ce72c
hahha <111@.com>
2015-10-08 19:09:52 +0800

    框架布局

--- MyWb/MyWb/AppDelegate.swift
+++ MyWb/MyWb/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = UIColor.redColor()
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- MyWb/MyWb/Class/Controllers/Main/MainViewController.swift
+++ MyWb/MyWb/Class/Controllers/Main/MainViewController.swift
@@ -12,14 +12,55 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addChildViewController(HomeTableViewController(), title: "首页", imagename:"tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imagename:"tabbar_message_center")
+        addChildViewController(UIViewController())
+        
+        addChildViewController(FinderTableViewController(), title: "查找", imagename:"tabbar_discover")
+        addChildViewController(MineTableViewController(), title: "我的", imagename:"tabbar_profile")
+        
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewDidLayoutSubviews() {
+       let btn = frameWithBtn()
+        tabBar.addSubview(btn)
+    }
+//添加子控制器
+    private func addChildViewController(v: UIViewController,title: String,imagename:String) {
+        v.title = title
+       tabBar.tintColor = UIColor.orangeColor()
+       v.tabBarItem.image = UIImage(named: imagename)
+        let nav = UINavigationController(rootViewController: v)
+       addChildViewController(nav)
+       
+    }
+//    设置控件位置
+    func frameWithBtn()->UIButton{
+        let W = tabBar.frame.width/5
+        
+    styleBtn.frame = CGRectMake(W*2, 0, W, tabBar.frame.height)
+     tabBar.addSubview(styleBtn)
+    return styleBtn
+    }
+//    懒加载控件
+    private lazy var styleBtn:UIButton = {
+    let btn = UIButton()
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
+        btn.addTarget(self, action: "clickButten", forControlEvents: UIControlEvents.TouchUpInside)
+        return btn
+    }()
+//    按钮的监听事件
+    func clickButten(){
     
-
+    print("点我")
+    
+    }
 }
